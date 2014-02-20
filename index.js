abbffa6e0b486b433bd4a6a2f043394f3af57af9
Toby Ho <airportyh@gmail.com>
2014-02-20 10:48:37 -0500

    Automatically rewritten for npm.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var empty = require('empty')
-	, append = require('append');
+var empty = require('yields-empty')
+	, append = require('bmcmahen-append');
 
 module.exports = function(el, content){
 	return append(empty(el), content);
